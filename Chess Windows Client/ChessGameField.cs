de5550b7aadb24212450eba4b5866b62d6b7082b
Pavel Grebnev <ruwhynot@gmail.com>
2016-07-30 01:33:21 +0300

    King base movement

--- Chess Windows Client/ChessGameField.cs
+++ Chess Windows Client/ChessGameField.cs
@@ -41,6 +41,9 @@ namespace Chess_Windows_Client
 			Field[5, 0].figure = new Bishop(Player.Black);
 			Field[2, 7].figure = new Bishop(Player.White);
 			Field[5, 7].figure = new Bishop(Player.White);
+
+			Field[3, 0].figure = new King(Player.Black);
+			Field[3, 7].figure = new King(Player.White);
 		}
 
 		public static Player GetOpponent(Player player)
---
+++ Chess Windows Client/Figures/Specific/King.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+
+namespace Chess_Windows_Client.Figures.Specific
+{
+	class King : FigureImpl, ChessFigure
+	{
+		public King(Player player) : base(player)
+		{
+		}
+
+		public void Draw(Graphics g, Color color, PointF pos, PointF size)
+		{
+			g.DrawString("K", SystemFonts.DefaultFont, new SolidBrush(color), pos);
+		}
+
+		public bool Move(ref ChessGameField.Cell[,] field, Point posFrom, Point posTo, ChessFigure lastMovedFig)
+		{
+			Figure targetFig = field[posTo.X, posTo.Y].figure;
+			if (targetFig == null || targetFig.GetOwner() != GetOwner())
+			{
+				int difX = Math.Abs(posTo.X - posFrom.X);
+				int difY = Math.Abs(posTo.Y - posFrom.Y);
+
+				if (difX <= 1 && difY <= 1)
+				{
+					field[posTo.X, posTo.Y].figure = this;
+					field[posFrom.X, posFrom.Y].figure = null;
+					return true;
+				}
+			}
+			return false;
+        }
+	}
+}
